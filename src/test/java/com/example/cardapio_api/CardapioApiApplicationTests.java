4e20f03e3e301db95c26b0932e6a1f3855350462
Natan <natan.ferreira21@hotmail.com>
2024-02-28 07:47:55 -0300

    Crirando projeto back-end FULLSTACK-Cardapio

---
+++ src/test/java/com/example/cardapio_api/CardapioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardapio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
