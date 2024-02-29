da9a1012ad8a87296c5dc21a6f22eba65dc749d8
Natan <natan.ferreira21@hotmail.com>
2024-02-28 23:04:45 -0300

    Cardapio API v:3 | terminado os EndPoints e Save

---
+++ src/main/java/com/example/cardapio_api/controller/FoodController.java
@@ -0,0 +1,33 @@
+package com.example.cardapio_api.controller;
+
+import com.example.cardapio_api.food.Food;
+import com.example.cardapio_api.food.FoodRepository;
+import com.example.cardapio_api.food.FoodRequestDTO;
+import com.example.cardapio_api.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
---
+++ src/main/java/com/example/cardapio_api/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.cardapio_api.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+}
