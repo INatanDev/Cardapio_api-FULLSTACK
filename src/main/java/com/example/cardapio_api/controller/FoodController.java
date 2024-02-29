b9867a2c4f05f2ab349f5b49b695a49e3fa4a99b
Natan <natan.ferreira21@hotmail.com>
2024-02-28 22:27:30 -0300

    Cardapio v:2 | Criado a entidade, repositorio e DTO

---
+++ src/main/java/com/example/cardapio_api/controller/FoodController.java
@@ -0,0 +1,26 @@
+package com.example.cardapio_api.controller;
+
+import com.example.cardapio_api.food.Food;
+import com.example.cardapio_api.food.FoodRepository;
+import com.example.cardapio_api.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
---
+++ src/main/java/com/example/cardapio_api/food/FoodResponseDTO.java
@@ -0,0 +1,7 @@
+package com.example.cardapio_api.food;
+
+public record FoodResponseDTO(Long id, String title, String image, Integer price) {
+    public FoodResponseDTO(Food food){
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+}
